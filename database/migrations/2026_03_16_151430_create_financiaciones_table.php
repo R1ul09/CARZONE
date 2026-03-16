2327febc986975dbd9963af11eb0d589e4dbcbc5
R1ul09 <128465165+R1ul09@users.noreply.github.com>
2026-03-16 16:39:02 +0100

    S1T2 - Creación de la base de datos y migraciones iniciales

---
+++ database/migrations/2026_03_16_151430_create_financiaciones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financiaciones', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('coche_id')->constrained('coches')->onDelete('cascade');
+
+            $table->integer('meses');
+            $table->decimal('cuota_mensual', 8, 2);
+            $table->decimal('entrada', 8, 2)->nullable();
+            $table->decimal('interes', 5, 2)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financiaciones');
+    }
+};
